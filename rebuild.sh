64b5c24b218cf331a9197893d54a39206cdb464c
repu1sion <bearrailgun@gmail.com>
2016-04-02 19:46:53 +0300

    fixed makefile and rebuild.sh to make tcpdump able to find libpcap

--- rebuild.sh
+++ rebuild.sh
@@ -5,8 +5,8 @@ NC='\033[0m' # No Color
 
 git pull
 
-if [ -d libpcap/odp_libpcap/ ]; then
-	rm -rf libpcap/odp_libpcap/
+if [ -d libpcap/libpcap/ ]; then
+	rm -rf libpcap/libpcap/
 	echo -e "${white}libpcap removed and will be rebuilt${NC}"
 else
 	echo -e "${white}libpcap were not found. will be downloaded and built${NC}"
