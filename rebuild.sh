06371308684a8edfc22726e61f54089f61867eef
repu1sion <bearrailgun@gmail.com>
2016-04-03 22:10:59 +0300

    removing tcpdump when run ./rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -12,4 +12,11 @@ else
 	echo -e "${white}libpcap were not found. will be downloaded and built${NC}"
 fi
 
+if [ -d libpcap/tcpdump/ ]; then
+	rm -rf libpcap/tcpdump/
+	echo -e "${white}tcpdump removed and will be rebuilt${NC}"
+else
+	echo -e "${white}tcpdump were not found. will be downloaded and built${NC}"
+fi
+
 make
